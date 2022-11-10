9b40e2a8ee933ea57f5832d7bbed80ac83a1af8f
Cwou <atoskuba@gmail.comm>
2022-11-10 16:06:46 +0100

    Koszykowka

---
+++ app/src/main/java/com/example/koszykowka/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.koszykowka;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.koszykowka.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    private PunktyViewModel punktyViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        punktyViewModel = new ViewModelProvider(this).get(PunktyViewModel.class);
+
+        punktyViewModel.getPunkty().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                binding.textView.setText(integer.toString());
+            }
+        });
+
+        binding.button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punktyViewModel.dodajPunkty(1);
+
+                    }
+                }
+        );
+
+        binding.button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punktyViewModel.dodajPunkty(2);
+
+                    }
+                }
+        );
+
+        binding.button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punktyViewModel.dodajPunkty(3);
+
+                    }
+                }
+        );
+
+    }
+}
